4a4b1555bb80335ad5966e83c8285c37ac466f7b
Iisakki Kulhua <iisakki@kulhua.net>
2025-02-20 14:33:33 +0200

    13.3

---
+++ blogs/commands.sql
@@ -0,0 +1,10 @@
+CREATE TABLE notes (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes int DEFAULT 0,
+);
+
+insert into blogs (author, url, title) values ('Dan Abramow', 'http://url.com', 'Writing Resilient Components');
+insert into blogs (author, url, title) values ('Martin Fowler', 'http://url.com', 'Is High Quality Software Worth the Cost?');
