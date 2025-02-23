77cbc451c867e3bae62c7ed95519e9619d46c323
Iisakki Kulhua <iisakki@kulhua.net>
2025-02-23 14:37:03 +0200

    13.19

--- blogs/models/index.js
+++ blogs/models/index.js
@@ -1,7 +1,11 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: ReadingList, as: 'markedBlogs' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'usersMarked' })
 
 module.exports = { Blog, User }
--- blogs/util/db.js
+++ blogs/util/db.js
@@ -4,19 +4,25 @@ const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL)
 
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: 'migrations/*.js',
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
+  const migrator = new Umzug(migrationConf)
   const migrations = await migrator.up()
   console.log('Migrations up to date', {
     files: migrations.map((mig) => mig.name),
   })
+}
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
 }
 
 const connectToDatabase = async () => {
@@ -32,4 +38,4 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { sequelize, connectToDatabase }
+module.exports = { sequelize, connectToDatabase, rollbackMigration }
